4ffb3264006422f7230d8dc211d8f558d095730c
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2022-10-30 18:42:31 -0500

    clase 5 terminada con la consulta desde db al entity

--- UCO.PracticaNET.ReglasNegocio/Model/CiudadModel.cs
+++ UCO.PracticaNET.ReglasNegocio/Model/CiudadModel.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UCO.PracticaNET.ReglasNegocio.Model
 {
-    internal class Class1
+    public class CiudadModel
     {
+        #region espacio para crear propiedades (atributos)
+        [DisplayName("Codigo de la ciudad")]
+        public int Id { get; set; }
+
+        [DisplayName("Nombre de la ciudad")]
+        public string Name { get; set; }
+
+
+        #endregion
+
+
+        #region espacio para metodos personalizados (metodos)
+        public List<CiudadModel> ConsultarCiudad ()
+        {
+            List<CiudadModel> listadoCiudad = new List<CiudadModel>();
+
+            //creo el objeto entidad
+            UCOPracticasNETEntities entidadDatos = new UCOPracticasNETEntities();
+            listadoCiudad = (from c in entidadDatos.ciudads
+                             select c).Select(registroCiudad => new CiudadModel
+                             {
+                                 Id = registroCiudad.id,
+                                 Name = registroCiudad.nombre
+
+                             }).ToList();
+            return null;
+
+        }
     }
+
+
+
+        #endregion
+
+
+
 }
